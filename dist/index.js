0eb8dfe946052edbc0bc3842a73497a0ad963faa
Darius Rosendahl <darius@denieuwezaak.nl>
2020-11-02 14:07:30 +0100

    - Added babel - Added babel to build script Moved index.ts to /src

--- dist/index.js
+++ dist/index.js
@@ -1,73 +1,122 @@
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 import { useState, useEffect, useRef, useImperativeHandle } from 'react';
 export function useHoverIntent(options) {
-    var ref = options.ref, _a = options.sensitivity, sensitivity = _a === void 0 ? 6 : _a, _b = options.interval, interval = _b === void 0 ? 100 : _b, _c = options.timeout, timeout = _c === void 0 ? 0 : _c;
-    var intentRef = useRef(null);
-    var _d = useState(false), isHovering = _d[0], setIsHovering = _d[1];
-    var x = 0, y = 0, pX = 0, pY = 0, timer = 0;
-    var delay = function (e) {
-        if (timer) {
-            clearTimeout(timer);
-        }
-        return setIsHovering(false);
+  var ref = options.ref,
+      _options$sensitivity = options.sensitivity,
+      sensitivity = _options$sensitivity === void 0 ? 6 : _options$sensitivity,
+      _options$interval = options.interval,
+      interval = _options$interval === void 0 ? 100 : _options$interval,
+      _options$timeout = options.timeout,
+      timeout = _options$timeout === void 0 ? 0 : _options$timeout;
+  var intentRef = useRef(null);
+
+  var _useState = useState(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      isHovering = _useState2[0],
+      setIsHovering = _useState2[1];
+
+  var x = 0,
+      y = 0,
+      pX = 0,
+      pY = 0,
+      timer = 0;
+
+  var delay = function delay(e) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    return setIsHovering(false);
+  };
+
+  var tracker = function tracker(e) {
+    x = e.clientX;
+    y = e.clientY;
+  };
+
+  var compare = function compare(e) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    if (Math.abs(pX - x) + Math.abs(pY - y) < sensitivity) {
+      return setIsHovering(true);
+    } else {
+      pX = x;
+      pY = y;
+      timer = window.setTimeout(function () {
+        return compare(e);
+      }, interval);
+    }
+  };
+
+  var dispatchOver = function dispatchOver(e) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    if (intentRef.current) {
+      intentRef.current.removeEventListener('mousemove', tracker, false);
+    }
+
+    if (!isHovering) {
+      pX = e.clientX;
+      pY = e.clientY;
+
+      if (intentRef.current) {
+        intentRef.current.addEventListener('mousemove', tracker, false);
+      }
+
+      timer = window.setTimeout(function () {
+        return compare(e);
+      }, interval);
+    }
+  };
+
+  var dispatchOut = function dispatchOut(e) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    if (intentRef.current) {
+      intentRef.current.removeEventListener('mousemove', tracker, false);
+    }
+
+    if (isHovering) {
+      timer = window.setTimeout(function () {
+        return delay(e);
+      }, timeout);
+    }
+  };
+
+  useEffect(function () {
+    var currentRef = intentRef.current;
+
+    if (currentRef) {
+      currentRef.addEventListener('mouseover', dispatchOver, false);
+      currentRef.addEventListener('mouseout', dispatchOut, false);
+    }
+
+    return function () {
+      if (currentRef) {
+        currentRef.removeEventListener('mouseover', dispatchOver, false);
+        currentRef.removeEventListener('mouseout', dispatchOut, false);
+      }
     };
-    var tracker = function (e) {
-        x = e.clientX;
-        y = e.clientY;
-    };
-    var compare = function (e) {
-        if (timer) {
-            clearTimeout(timer);
-        }
-        if (Math.abs(pX - x) + Math.abs(pY - y) < sensitivity) {
-            return setIsHovering(true);
-        }
-        else {
-            pX = x;
-            pY = y;
-            timer = window.setTimeout(function () { return compare(e); }, interval);
-        }
-    };
-    var dispatchOver = function (e) {
-        if (timer) {
-            clearTimeout(timer);
-        }
-        if (intentRef.current) {
-            intentRef.current.removeEventListener('mousemove', tracker, false);
-        }
-        if (!isHovering) {
-            pX = e.clientX;
-            pY = e.clientY;
-            if (intentRef.current) {
-                intentRef.current.addEventListener('mousemove', tracker, false);
-            }
-            timer = window.setTimeout(function () { return compare(e); }, interval);
-        }
-    };
-    var dispatchOut = function (e) {
-        if (timer) {
-            clearTimeout(timer);
-        }
-        if (intentRef.current) {
-            intentRef.current.removeEventListener('mousemove', tracker, false);
-        }
-        if (isHovering) {
-            timer = window.setTimeout(function () { return delay(e); }, timeout);
-        }
-    };
-    useEffect(function () {
-        var currentRef = intentRef.current;
-        if (currentRef) {
-            currentRef.addEventListener('mouseover', dispatchOver, false);
-            currentRef.addEventListener('mouseout', dispatchOut, false);
-        }
-        return function () {
-            if (currentRef) {
-                currentRef.removeEventListener('mouseover', dispatchOver, false);
-                currentRef.removeEventListener('mouseout', dispatchOut, false);
-            }
-        };
-    });
-    useImperativeHandle(ref, function () { return intentRef.current; }, [intentRef]);
-    return [isHovering, intentRef];
+  });
+  useImperativeHandle(ref, function () {
+    return intentRef.current;
+  }, [intentRef]);
+  return [isHovering, intentRef];
 }
-//# sourceMappingURL=index.js.map
